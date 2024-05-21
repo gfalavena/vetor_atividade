fc5bb1c121927e50988ef3749fc6777987ba61f5
Gabriel <gabriel.oliveira1370@etec.sp.gov.br>
2024-05-21 14:52:46 -0300

    primeiro commit da atividade

---
+++ Atividade_Vetor/Cliente.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade_Vetor
+{
+    internal class Cliente
+    {
+        public Cliente[] clien;
+        public string nome;
+        public int idade;
+        public string rg;
+        public string cpf;
+        public int qntdfila = 0;
+        public int prioridade = 0;
+
+        public Cliente(string nome = "", int idade = 0, string rg = "", string cpf = "", bool prioridade = false)
+        {
+            clien = new Cliente[10];
+            this.nome = nome;
+            this.idade = idade;
+            this.rg = rg;
+            this.cpf = cpf;
+            this.prioridade = 0;
+        }
+
+
+        public void CadastrarCliente()
+        {
+            if (qntdfila < 10)
+            {
+                Console.WriteLine("\nDigite o seu nome: ");
+                nome = Console.ReadLine();
+                Console.WriteLine("Digite a sua idade: ");
+                idade = int.Parse(Console.ReadLine());
+                Console.WriteLine("Digite o seu rg: ");
+                rg = Console.ReadLine();
+                Console.WriteLine("Digite o seu cpf: ");
+                cpf = Console.ReadLine();
+                Console.WriteLine("Paciente cadastrado com sucesso!");
+                Cliente cliente = new Cliente(nome, idade, rg, cpf);
+                clien[qntdfila] = cliente;
+                qntdfila++;
+            }
+        }
+
+
+
+
+        public void ListarCliente()
+        {
+
+            if (qntdfila > 0)
+            {
+                for (int i = 0; i < clien.Length; i++)
+                {
+                    if (clien[i] != null)
+                    {
+                        Console.WriteLine("\n\nCliente:{0}", i + 1);
+                        Console.WriteLine("Nome:" + clien[i].nome);
+                        Console.WriteLine("Idade:" + clien[i].idade);
+                        Console.WriteLine("RG:" + clien[i].rg);
+                        Console.WriteLine("CPF:" + clien[i].cpf);
+                        Console.WriteLine("Prioridade:" + clien[i].prioridade);
+                        Console.WriteLine("Clientes na fila:" + qntdfila);
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Fila esta vazia!");
+            }
+        }
+
+        public void IncluirClientePrioritario()
+        {
+            if (qntdfila < 10)
+            {
+                Console.WriteLine("\nDigite o seu nome: ");
+                nome = Console.ReadLine();
+                Console.WriteLine("Digite a sua idade: ");
+                idade = int.Parse(Console.ReadLine());
+                Console.WriteLine("Digite o seu rg: ");
+                rg = Console.ReadLine();
+                Console.WriteLine("Digite o seu cpf: ");
+                cpf = Console.ReadLine();
+                Cliente clienteprioriário = new Cliente(nome, idade, rg, cpf, true);
+
+                for (int j = qntdfila - 1; j >= prioridade; j--)
+                {
+                    clien[j + 1] = clien[j];
+                }
+                clien[prioridade] = clienteprioriário; prioridade++; qntdfila++;
+            }
+
+        }
+
+        public void RetirarCliente()
+        {
+            Cliente retirarCliente = clien[0];
+
+            if (qntdfila > 0)
+            {
+                for (int i = 1; i < qntdfila; i++)
+                {
+                    clien[i - 1] = clien[i];
+                }
+
+                clien[qntdfila - 1] = null;
+                qntdfila--;
+
+                Console.WriteLine($"\n\nO cliente {retirarCliente.nome} foi retirado com sucesso!");
+            }
+
+        }
+    }
+}
+
---
+++ Atividade_Vetor/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade_Vetor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cliente cliente = new Cliente();
+            char opcao = '1';
+
+            Console.WriteLine("\t\t\t\t\tSISTEMA BANCÁRIO\t\t\t\t\t");
+            while (opcao != 'q')
+            {
+                Console.WriteLine("\n\n\nBem vindo ao sistema de gerenciamento do banco. Escolha uma das opções abaixo: \n");
+                Console.WriteLine("1.Cadastrar/Inserir\t  2.Listar\t 3.Incluir cliente prioritário\t  4.Retirar o cliente\t  6.(Q) Sair do Programa\t");
+                opcao = Console.ReadKey().KeyChar;
+
+                switch (opcao)
+                {
+
+                    case '1':
+
+                        cliente.CadastrarCliente();
+                        break;
+
+
+                    case '2':
+
+                        cliente.ListarCliente();
+                        break;
+
+                    case '3':
+
+                        cliente.IncluirClientePrioritario();
+                        break;
+
+                    case '4':
+
+                        cliente.RetirarCliente();
+                        break;
+
+                    case 'Q':
+                        Environment.Exit(1);
+                        break;
+
+                }
+
+            }
+        }
+    }
+}
